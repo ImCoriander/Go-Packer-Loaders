934070b06af639099663a05501c529c9b664831b
生吃香菜 <165987738+ImCoriander@users.noreply.github.com>
2024-08-23 16:25:06 +0800

    Create config.go

---
+++ config.go
@@ -0,0 +1,5 @@
+package Loaders
+
+var fruits = []string{"CertEnumSystemStore", "CertEnumSystemStoreLocation", "CopyFile2", "CopyFileEx", "CreateThreadPoolWait", "CreateTimerQueueTimer_Tech", "CryptEnumOIDInfo", "EnumCalendarInfo", "EnumCalendarInfoEX", "EnumChildWindows", "EnumDesktopW", "EnumDesktopWindows", "EnumDirTreeW", "EnumDisplayMonitors", "EnumerateLoadedModules", "EnumFontFamiliesExW", "EnumFontFamiliesW", "EnumFontsW", "EnumICMProfiles", "EnumLanguageGroupLocalesW", "EnumObjects", "EnumPageFilesW", "EnumPropsEx", "EnumPropsW", "EnumPwrSchemes", "EnumResourceTypesExW", "EnumResourceTypesW", "EnumSystemLocales", "EnumThreadWindows", "EnumTimeFormatsEx", "EnumUILanguagesW", "EnumWindows", "EnumWindowStationsW", "FiberContextEdit", "FlsAlloc", "ImageGetDigestStream", "ImmEnumInputContext", "InitOnceExecuteOnce", "LdrEnumerateLoadedModules", "LdrpCallInitRoutine", "RtlUserFiberStart", "SetTimer", "SetupCommitFileQueueW", "SymEnumProcesses", "SymFindFileInPath", "SysEnumSourceFiles"}
+
+var Version = "1.0"
